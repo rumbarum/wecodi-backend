95433668211386dbbdbeec10b5e04015c74e3fac
Bareum Han <rumbarum@gmail.com>
2019-08-27 10:56:33 +0900

    minor fix

--- wecodi_backend/settings.py
+++ wecodi_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from . import my_setting
+from . import my_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
