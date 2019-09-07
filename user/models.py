d6ce75b4eb7112ec086ab41071efbab88156bd34
Bareum Han <rumbarum@gmail.com>
2019-09-07 21:42:15 +0900

    article fix

--- user/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class UserModel(models.Model):
-    user_id = models.CharField(max_length=40)
-    user_pw = models.CharField(max_length=40)
--- user/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import UserLogin
-
-urlpatterns = [
-    path('/login', UserLogin.as_view()),
-]
